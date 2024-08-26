98677ed4dda5b14369cb79d1089272e61baecc7e
liyaka <liyak@comet.com>
2024-08-26 17:13:21 +0300

    add app

---
+++ app/app.py
@@ -0,0 +1,20 @@
+""" Flask App """
+import time
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_message():
+    """ Get the message """
+    current_timestamp = int(time.time())
+    message_payload = {
+        "message": "Hello World!",
+        "timestamp": current_timestamp
+    }
+    return jsonify(message_payload)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
